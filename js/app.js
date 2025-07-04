89bec667db74e8908c8e34330243b1d7bd9951ae
Victor Hugo de França de Pinho <victorfpinho@bb.com.br>
2025-07-04 15:22:51 -0300

    Criar Funções adicionar, sortear e embaralhar

---
+++ js/app.js
@@ -0,0 +1,28 @@
+
+let amigos = [];
+
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo');
+    let lista = document.getElementById('lista-amigos');
+    amigos.push(amigo.value);
+
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
+    amigo.value = ''
+
+}
+
+function sortear() {
+    embaralhar(amigos);
+}
+
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
